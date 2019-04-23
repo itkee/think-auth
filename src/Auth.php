ff77a4f7db600eaeea508f9a336f82fc5999dfa1
SuperMan <itkee@vip.qq.com>
2019-04-23 11:46:29 +0800

    change

--- src/Auth.php
+++ src/Auth.php
@@ -242,4 +242,41 @@ class Auth
 
         return $userinfo[$uid];
     }
+
+    /**
+     * 把返回的数据集转换成Tree
+     * @access public
+     * @param array $list 要转换的数据集
+     * @param string $pid parent标记字段
+     * @param string $level level标记字段
+     * @return array
+     */
+    public function list_to_tree($list, $pk='id', $pid = 'pid', $child = '_child', $root = 0, $is_count = false) {
+        // 创建Tree
+        $tree = [];
+        if(is_array($list)) {
+            // 创建基于主键的数组引用
+            $refer = [];
+            foreach ($list as $key => $data) {
+                $refer[$data[$pk]] =& $list[$key];
+            }
+
+            foreach ($list as $key => $data) {
+                // 判断是否存在parent
+                $parentId =  $data[$pid];
+                if ($root == $parentId) {
+                    $tree[] =& $list[$key];
+                }else{
+                    if (isset($refer[$parentId])) {
+                        $parent =& $refer[$parentId];
+                        $parent[$child][] =& $list[$key];
+                        if($is_count==true) $parent['_count'] = count($parent[$child]);
+                    }
+                }
+            }
+        }
+        return $tree;
+    }
+
+
 }
